c9d5d6499e1dd000a9ed392bbde932c3e3a9baa7
Alparslan Tarkan <atarkan@hotmail.com>
2024-10-21 06:42:28 +0200

    a basic variable exercise is added

---
+++ app/src/main/java/com/example/kotlinbasics/VariableTypes.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinbasics
+
+fun main() {
+    val constantNumber = 15
+    var mutableString = "Hello World"
+    mutableString = "Hello World Alpy"
+    println(constantNumber)
+    println(mutableString)
+}
+
+
+
+
